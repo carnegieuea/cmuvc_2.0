339e08b8c5fc623869ba5d6b5e57dab8e5e554d2
Samarth Gowda <samarthgowda@outlook.com>
2021-04-03 11:38:26 -0400

    Updated schedule of events

--- src/containers/demoday/index.js
+++ src/containers/demoday/index.js
@@ -14,31 +14,6 @@ const DemoDay = () => {
                     <h1 className="display-5 font-weight-bolder text-black">
                     Demo Day 2021.
                     </h1>
-                    <p className="lead">The link to view the CMU Venture Challenge 2021 Powered By Accel Demo Day will be released shortly.</p>
-
-                    {/* <Button
-                        tag="a"
-                        disabled
-                        href="https://www.pralent.com/challenges/cmuvc-2021"
-                        color="warning"
-                        size="md"
-                        className="mx-1"
-                        target="_blank"
-                        rel="noreferrer noopener"
-                    >
-                        View Demo Day
-                    </Button> */}
-                    <div className="pt-4">
-                        <h6 className="font-weight-bold">SCHEDULE OF EVENTS (EST)</h6>
-                        <ul className="list-unstyled font-weight-bold text-info">
-                            <li className="py-1">12pm : Introductions + Keynote with Michael Seibel</li>
-                            <li className="py-1">12:30pm : Startup Pitches (Part I)</li>
-                            <li className="py-1">2:00pm : Lunch Break</li>
-                            <li className="py-1">2:30pm : Startup Pitches (Part II)</li>
-                            <li className="py-1">4:00pm : Networking Session and Judges Deliberation</li>
-                            <li className="py-1">4:30pm : Awards</li>
-                        </ul>
-                    </div>
                 </Col>
             </Row>
         </Container>
@@ -57,6 +32,19 @@ const DemoDay = () => {
                 >
                     View Demo Day
             </Button>
+        </div>
+        <div>
+        <Container className="pt-4 text-center">
+            <h6 className="font-weight-bold">SCHEDULE OF EVENTS (EST)</h6>
+            <ul className="list-unstyled font-weight-bold text-info">
+                <li className="py-1">12pm : Introductions + Keynote with Michael Seibel</li>
+                <li className="py-1">12:30pm : Startup Pitches (Part I)</li>
+                <li className="py-1">2:00pm : Lunch Break</li>
+                <li className="py-1">2:30pm : Startup Pitches (Part II)</li>
+                <li className="py-1">4:00pm : Networking Session and Judges Deliberation</li>
+                <li className="py-1">4:30pm : Awards</li>
+            </ul>
+        </Container>
         </div>
         </>
     )
