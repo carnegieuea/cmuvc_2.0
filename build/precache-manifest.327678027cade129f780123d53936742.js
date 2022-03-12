21c2ec1d68c430a9aa761e021a30437199a55f30
Andrew Zhao <andrewhzhao@gmail.com>
2022-03-12 14:11:32 -0500

    build updated 2

---
+++ build/precache-manifest.327678027cade129f780123d53936742.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4b34c4549c2c3b7ac3087b9b4c68c08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b0e7a6e20aefb344b74",
+    "url": "/static/css/2.73a3c38b.chunk.css"
+  },
+  {
+    "revision": "21b47282dc880b80d5ce",
+    "url": "/static/css/main.8a559013.chunk.css"
+  },
+  {
+    "revision": "0b0e7a6e20aefb344b74",
+    "url": "/static/js/2.70fb0dfe.chunk.js"
+  },
+  {
+    "revision": "21b47282dc880b80d5ce",
+    "url": "/static/js/main.f3d1161b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b3bd1069d9fa1ac892d947ff392b801b",
+    "url": "/static/media/AikenHouse_black.b3bd1069.png"
+  },
+  {
+    "revision": "91e1ffa67e33dff7e76fd4795b7d3e68",
+    "url": "/static/media/Arleen.91e1ffa6.jpg"
+  },
+  {
+    "revision": "0c8cf428c2f4bb2452586f699278dc97",
+    "url": "/static/media/Avika.0c8cf428.jpg"
+  },
+  {
+    "revision": "11b90e0a9f82c6660f6a4581d1bca884",
+    "url": "/static/media/Eric.11b90e0a.jpg"
+  },
+  {
+    "revision": "2976073c330fffa7b34116cc98e76b9c",
+    "url": "/static/media/Snehita.2976073c.jpg"
+  },
+  {
+    "revision": "0388f75dd2988dbcec38cefbad97dc89",
+    "url": "/static/media/accel-white.0388f75d.svg"
+  },
+  {
+    "revision": "d37a5ab4343e1d518dc5511cbe748f46",
+    "url": "/static/media/accel.d37a5ab4.png"
+  },
+  {
+    "revision": "8f676b22ea9525edc5ebf915d294d175",
+    "url": "/static/media/alpha_lab.8f676b22.png"
+  },
+  {
+    "revision": "c7de0ba5d300bb79b121650ef05f0cc2",
+    "url": "/static/media/angela.c7de0ba5.jpg"
+  },
+  {
+    "revision": "2cbbfac5e62602392e74d33e66070961",
+    "url": "/static/media/blue_tree1.2cbbfac5.png"
+  },
+  {
+    "revision": "0696577914a17771b1d50fb19c741ffe",
+    "url": "/static/media/campusvc.06965779.png"
+  },
+  {
+    "revision": "822c8ef0f47e82ce4e51043f027165b7",
+    "url": "/static/media/choolaah.822c8ef0.jpg"
+  },
+  {
+    "revision": "f4a0424b5fca7a05834f89b7b77cf4b1",
+    "url": "/static/media/dietrich.f4a0424b.png"
+  },
+  {
+    "revision": "a8ce34328a4dbd1932d4acaa5ad2ac58",
+    "url": "/static/media/drf.a8ce3432.png"
+  },
+  {
+    "revision": "d44b32980b0b16bf1414bb85e6ee3f98",
+    "url": "/static/media/duolingo.d44b3298.png"
+  },
+  {
+    "revision": "f01f564412a6df234b22490886a517a7",
+    "url": "/static/media/generalcatalyst.f01f5644.jpg"
+  },
+  {
+    "revision": "67713003763f0a7e83f9f22ba491114d",
+    "url": "/static/media/ggv.67713003.png"
+  },
+  {
+    "revision": "2dca97949aadd5a510994d5700dcbcef",
+    "url": "/static/media/grouseridge.2dca9794.jpg"
+  },
+  {
+    "revision": "7a34b1193b3db2e19f4aa36cbe0fc916",
+    "url": "/static/media/hero.7a34b119.png"
+  },
+  {
+    "revision": "8d8575492e3acc617957c46fc20982f7",
+    "url": "/static/media/innospring.8d857549.jpg"
+  },
+  {
+    "revision": "fd6d83a1b1311967d5095a6c04f166fd",
+    "url": "/static/media/innovation_works.fd6d83a1.png"
+  },
+  {
+    "revision": "534caea94a1ccf05f1bf966f1d8571ba",
+    "url": "/static/media/michael.534caea9.png"
+  },
+  {
+    "revision": "49beecb1ac8fd0c99a6785b30d35476b",
+    "url": "/static/media/muslaw.49beecb1.jpeg"
+  },
+  {
+    "revision": "4ab71193259f21a6573c11c2336febc2",
+    "url": "/static/media/pvca.4ab71193.jpg"
+  },
+  {
+    "revision": "01446651bcfb3dc404435993e67d683a",
+    "url": "/static/media/rdv.01446651.png"
+  },
+  {
+    "revision": "3b646d420d70484cb994bd1045ea6633",
+    "url": "/static/media/samarth.3b646d42.JPG"
+  },
+  {
+    "revision": "a81151dbda290359c8f90d04dfad1c3d",
+    "url": "/static/media/scs1.a81151db.png"
+  },
+  {
+    "revision": "5436515d765cb26bd5d4a64d72c16c41",
+    "url": "/static/media/sdc.5436515d.png"
+  },
+  {
+    "revision": "129e6396368c2509b705220dce06ceae",
+    "url": "/static/media/senate.129e6396.jpg"
+  },
+  {
+    "revision": "c4c6a360e028aa8c65baae2c2a033fd3",
+    "url": "/static/media/sequoia.c4c6a360.jpeg"
+  },
+  {
+    "revision": "516431a2c415e62526b5c2a44db5ebe8",
+    "url": "/static/media/swartz.516431a2.png"
+  },
+  {
+    "revision": "6b7dd17a085826ed8fb855ccbe587afd",
+    "url": "/static/media/tepper_logo.6b7dd17a.png"
+  },
+  {
+    "revision": "9f579d018bbd4ca1adf506d3a34a7ad7",
+    "url": "/static/media/two-sigma-ventures.9f579d01.png"
+  }
+]);
