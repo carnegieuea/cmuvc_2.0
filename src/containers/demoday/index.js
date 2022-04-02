ea997eec1f8f0b86dfdfa1ec9ee7f3d2ef131d70
Eric Liang <39630822+liangeric@users.noreply.github.com>
2022-04-02 12:08:54 -0400

    Updated Youtube URL

--- src/containers/demoday/index.js
+++ src/containers/demoday/index.js
@@ -18,7 +18,7 @@ const DemoDay = () => {
           title="cmuvc-demo-day"
           width="560"
           height="315"
-          src="https://www.youtube.com/embed/I10GdZ__V9E"
+          src="https://www.youtube.com/watch?v=Jhx9OJPJWAc"
           frameborder="0"
           allowfullscreen
         ></iframe>
